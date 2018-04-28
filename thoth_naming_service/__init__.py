c0cd975abecec75ab959e13bb58538344cadcc30
Christoph Görn <goern@redhat.com>
2018-04-28 13:06:14 +0200

    bouncing version
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- thoth_naming_service/__init__.py
+++ thoth_naming_service/__init__.py
@@ -1,6 +1,6 @@
 import os
 
 
-__version__ = '0.4.0'
+__version__ = '0.3.0'
 __description__ = 'Thoth: Naming Service, ask me if you want to find things...'
 __git_commit_id__ = os.getenv('OPENSHIFT_BUILD_COMMIT', 'local')
